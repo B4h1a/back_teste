c6e23b1568259c7566e7502df795b96ee191f62a
b4h1a <victorhugobahia10@gmail.com>
2024-09-20 09:04:02 -0300

    teste de api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import {getAllProducts} from '../services/productService.js';
+import {getAllProducts, createProduct, getProductById} from '../services/productService.js';
 
 export const  getProducts = async(req, res) => {
     try{
@@ -9,5 +9,30 @@ export const  getProducts = async(req, res) => {
         res.status(500).json({
             error: "Erro ao Buscar produtos",
         });
+    }
+};
+
+export const createProductController = async (req,res) => {
+    try{
+        const newProduct = await createProduct(req.body);
+        res.status(201).json(newProduct)
+    }
+    catch(error) {
+        res.status(500).json({
+            error:"Deu pau né",
+        })
+    }
+}
+
+export const getProduct = async(req,res) => {
+    try{
+        const product = await getProductById(req.params.id)
+        if(product) {
+            res.json(product) 
+        } else {
+            res.status(500).json({error: "Deu erro né"});
+        }
+    } catch (error) {
+        res.status(500).jaon({ error: "Error ao buscar produto"});
     }
 }
